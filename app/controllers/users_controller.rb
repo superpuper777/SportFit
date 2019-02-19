0bf34d4232325742c47c2c9cade24fde8ac5e87a
superpuper777 <superfevrik@gmail.com>
2019-02-19 16:11:32 +0300

    user model and login and registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+  @user = User.new(user_params)
+  if @user.save
+    log_in @user
+    flash[:success] = "Welcome!"
+    redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :age, :weight, :height, :email, :password, :password_confirmation)
+  end
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+end
